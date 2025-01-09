1f7f121e74afe51512640b367d0f9293f4398128
ElizavetaSM <elidream99@gmail.com>
2025-01-09 19:59:01 +0300

    Update Keyboard.java

--- src/main/java/homeworks/homeworks04/Keyboard.java
+++ src/main/java/homeworks/homeworks04/Keyboard.java
@@ -2,10 +2,18 @@ package homeworks.homeworks04;
 
 public class Keyboard {
     public static void main(String[] args) {
-        // String alhabet = "qwertyuiopasdfghjklzxcvbnm";
-        StringBuilder alhabet = new StringBuilder("qwertyuiopasdfghjklzxcvbnm");
-        alhabet.replace(0, 27, "mqwertyuiopasdfghjklzxcvbn");
-        System.out.println(alhabet);
+        String alhabet = "qwertyuiopasdfghjklzxcvbnm";
+        char [] chars = alhabet.toCharArray();
+        Scanner scanner = new Scanner(System.in);
+        String letter = scanner.next();
+        int index = alhabet.indexOf(letter);
+        boolean containChar;
+        if (alhabet.contains(String.copyValueOf(letter.toCharArray()))) {
+            containChar = true;
+            if (index == 0) {
+                System.out.println(alhabet.charAt(25));}
+            else {
+                System.out.println(alhabet.charAt(index-1));}
     }
 }
 
