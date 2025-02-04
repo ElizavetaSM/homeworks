09698d3b67cf8080ecaa52e3b472a46000f2bab1
ElizavetaSM <elidream99@gmail.com>
2025-02-04 14:32:58 +0300

    homework09 темы«Инкапсуляция. Модификаторы доступа в Java»

---
+++ src/main/java/homeworks/homework09/races/Race.java
@@ -0,0 +1,80 @@
+package homeworks.homework09.races;
+
+import homeworks.homework09.cars.Car;
+import homeworks.homework09.cars.PerformanceCar;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Race {
+    private int routeLenght;
+    private String route;
+    private int prizeFund;
+    private Car[] carsGallery;
+
+
+    public Race(int routeLenght, String route, int prizeFund, Car[] carsGallery) {
+        this.routeLenght = routeLenght;
+        this.route = route;
+        this.prizeFund = prizeFund;
+        this.carsGallery = carsGallery;
+    }
+
+    public Race() {
+    }
+
+    @Override
+    public String toString() {
+        return "Race{" +
+                "routeLenght=" + routeLenght +
+                ", route='" + route + '\'' +
+                ", prizeFund=" + prizeFund +
+                ", carsGallery=" + Arrays.toString(carsGallery) +
+                '}';
+    }
+
+    public int getRouteLenght() {
+        return routeLenght;
+    }
+
+    public void setRouteLenght(int routeLenght) {
+        this.routeLenght = routeLenght;
+    }
+
+    public String getRoute() {
+        return route;
+    }
+
+    public void setRoute(String route) {
+        this.route = route;
+    }
+
+    public int getPrizeFund() {
+        return prizeFund;
+    }
+
+    public void setPrizeFund(int prizeFund) {
+        this.prizeFund = prizeFund;
+    }
+
+    public Car[] getCarsGallery() {
+        return carsGallery;
+    }
+
+    public void setCarsGallery(Car[] carsGallery) {
+        this.carsGallery = carsGallery;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Race race = (Race) o;
+        return routeLenght == race.routeLenght && prizeFund == race.prizeFund && Objects.equals(route, race.route) && Objects.deepEquals(carsGallery, race.carsGallery);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(routeLenght, route, prizeFund, Arrays.hashCode(carsGallery));
+    }
+}
