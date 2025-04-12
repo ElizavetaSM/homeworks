b6c90c05235a011c0de794f0bf736de1a62695f4
ElizavetaSM <elidream99@gmail.com>
2025-04-12 20:53:37 +0300

    JDBC, JPA

---
+++ src/main/java/homeworks/homeworkJPA/src/main/resources/test-queries.sql
@@ -0,0 +1,21 @@
+-- 1.  Получение всех покупателей
+SELECT * FROM Users;
+
+-- 2. Получение всех заказов
+SELECT * FROM Orders;
+
+-- 3. Удаление всех покупателей
+DELETE FROM Users;
+
+-- 4. Удаление всех заказов
+DELETE FROM Orders;
+
+-- 5.
+SELECT * FROM Users WHERE id = 1;
+
+-- 6.
+SELECT * FROM Orders WHERE id = 02;
+
+-- 7.
+SELECT * FROM Users WHERE order_date = '2025-04-02';
+
